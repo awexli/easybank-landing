8f0983be95061fc62788499c43b89675008ca3a7
awexli <awexli@mail.sfsu.edu>
2020-05-12 00:26:01 -0700

    setup vue

---
+++ easybank_landing/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <navigation />
+    <attribution />
+  </div>
+</template>
+
+<script>
+import navigation from "./components/navigation.vue";
+import attribution from "./components/attribution.vue";
+
+export default {
+  name: "App",
+  components: {
+    navigation,
+    attribution
+  }
+};
+</script>
+
+<style lang="scss">
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100vh;
+}
+
+body {
+  font-size: 18px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ easybank_landing/src/components/attribution.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="attribution">
+    Challenge by
+    <a
+      href="https://www.frontendmentor.io?ref=challenge"
+      target="_blank"
+      rel="noopener"
+      >Frontend Mentor</a
+    >. Coded by <a href="#">Your Name Here</a>.
+  </div>
+</template>
+
+<script>
+export default {
+  name: "attribution"
+};
+</script>
+
+<style lang="scss" scoped>
+.attribution {
+  font-size: 11px;
+  text-align: center;
+}
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+</style>
