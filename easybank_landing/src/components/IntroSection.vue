d747ed58fbe95360840b746d2857ef870fadd06d
awexli <awexli@mail.sfsu.edu>
2020-05-15 21:35:09 -0700

    leave intro mock background alone for now

--- easybank_landing/src/components/IntroSection.vue
+++ easybank_landing/src/components/IntroSection.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="intro">
-    <img src="../assets/bg-intro-mobile.svg" alt="Background intro" class="intro__bg" />
-    <h1 class="intro__title">Next generation digital banking</h1>
-    <p class="intro__description">
-      Take your financial life online. Your Easybank account will be a
-      one-stop-shop for spending, saving, budgeting, investing, and much more.
-    </p>
+    <div class="intro__right">
+      <div class="intro__mock-container">
+        <img src="../assets/image-mockups.png" alt="Mockups" class="intro__mock" />
+      </div>
+    </div>
+    <div class="intro__left">
+      <h1 class="intro__title">Next generation digital banking</h1>
+      <p class="intro__description">
+        Take your financial life online. Your Easybank account will be a
+        one-stop-shop for spending, saving, budgeting, investing, and much more.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -20,9 +26,25 @@ export default {
 
 <style lang="scss" scoped>
 .intro {
-  &__bg {
+  display: grid;
+
+  &__right {
+    background: url(../assets/bg-intro-mobile.svg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+
+  &__mock-container {
     position: relative;
-    width: 100%;
+  }
+
+  &__mock {
+    max-width:100%;
+  }
+
+  &__left {
+    background-color: wheat;
   }
 
   &__title {
