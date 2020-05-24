c0faf1308c3816e7ab8bf98b5c4ca38524c88eb8
awexli <awexli@mail.sfsu.edu>
2020-05-23 18:37:02 -0700

    fix nav accessibility

--- src/main.js
+++ src/main.js
@@ -8,12 +8,19 @@ new Vue({
 }).$mount("#app");
 
 (function() {
-  // let isMobileMenuOpen = false;
+  let isMobileMenuOpen = false;
   const overlay = document.querySelector(".overlay");
   const burger = document.querySelector(".nav-bar__burger");
 
   document.querySelector(".nav-bar__btn").addEventListener("click", () => {
-    overlay.classList.toggle("active");
-    burger.classList.toggle("active");
+    if (!isMobileMenuOpen) {
+      overlay.className += " active";
+      burger.className += " active";
+      isMobileMenuOpen = true;
+    } else {
+      overlay.classList.remove("active");
+      burger.classList.remove("active");
+      isMobileMenuOpen = false;
+    }
   });
 })();
